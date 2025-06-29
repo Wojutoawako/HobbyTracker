a90154b8842d94bd6588f2856e7d7a9b4daf016f
Егор Чижов <123ega2@gmail.com>
2025-06-29 20:37:19 +0500

    Расширение ContentPage для сохранения и загрузки из Application

---
+++ MobileAppProject/ContentPageExtension.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace MobileAppProject
+{
+    public class ContentPageExtension : ContentPage
+    {
+        public virtual async void SavePageData()
+        {
+        }
+
+        public virtual async void LoadPageData()
+        {
+        }
+
+        public virtual async void RemoveData()
+        {
+        }
+    }
+}
