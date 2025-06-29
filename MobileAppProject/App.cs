e7acfa7596bf045148c4f211cd2acf73a87714e0
Егор Чижов <123ega2@gmail.com>
2025-06-29 20:36:35 +0500

    Сохранение и загрузка на уходе в сон и на старте соответственно

--- MobileAppProject/App.cs
+++ MobileAppProject/App.cs
@@ -1,15 +1,17 @@
-﻿using Newtonsoft.Json;
-using PCLStorage;
-using System.Collections.ObjectModel;
-using System.IO;
+﻿using PCLStorage;
 using Xamarin.Forms;
 
 namespace MobileAppProject
 {
     public partial class App : Application
     {
+        public static IFolder RootFolder = FileSystem.Current.LocalStorage;
+        public static IFolder SaveFolder;
+
         public App()
         {
+            SaveFolder = RootFolder.CreateFolderAsync("HTSave", CreationCollisionOption.OpenIfExists).Result;
+
             Current.Resources.Add(Styles.LargeButtonStyle);
 
             MainPage = new NavigationPage(new MainPage());
@@ -17,10 +19,24 @@ namespace MobileAppProject
 
         protected override void OnStart()
         {
+            var mainPage = (MainPage as NavigationPage).RootPage as TabbedPage;
+
+            foreach (var page in mainPage.Children)
+            {
+                if (page is ContentPageExtension pageExtended)
+                    pageExtended?.LoadPageData();
+            }
         }
 
-        protected override async void OnSleep()
+        protected override void OnSleep()
         {
+            var mainPage = (MainPage as NavigationPage).RootPage as TabbedPage;
+
+            foreach (var page in mainPage.Children)
+            {
+                if (page is ContentPageExtension pageExtended)
+                    pageExtended?.SavePageData();
+            }
         }
 
         protected override void OnResume()
